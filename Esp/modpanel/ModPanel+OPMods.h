56f79b543f24e7693e9d7a1344c80d38878b4627
ghxstfrl <wymwdim@gmail.com>
2026-02-23 11:59:18 -0600

    Update ModPanel+OPMods.h

--- Esp/modpanel/ModPanel+OPMods.h
+++ Esp/modpanel/ModPanel+OPMods.h
@@ -1,1 +1,10 @@
+#import "ModPanel.h"
 
+@interface ModPanel (OPMods)
+
+// Functions for your 30 OP mods
+- (void)activateExtraEXP;
+- (void)activateGodMode;
+- (void)activateAllOPMods; // optional, calls all mods
+
+@end
