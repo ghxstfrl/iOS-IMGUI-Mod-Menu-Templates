33c391b694c173b4e0ab281a5a03b1cf1e07449b
ghxstfrl <wymwdim@gmail.com>
2026-02-23 12:52:09 -0600

    Update PubgLoad.mm

--- Esp/PubgLoad.mm
+++ Esp/PubgLoad.mm
@@ -3,17 +3,16 @@
 #import "ImGuiDrawView.h"
 #import "CaptainHook.h"
 
-// 2. Wait for the app to finish launching, then show the ImGui menu
+// Wait for the app to finish launching, then show the M1 ImGui menu
 static void didFinishLaunching(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef info) {
     
-    // Wait 3 seconds to ensure the game has created the main window
+    // Wait 3 seconds to ensure the game has created the main window, then launch the UI
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [ImGuiDrawView showMenu];
     });
 }
 
-// 1. This constructor runs instantly when the tweak is injected
+// This constructor runs instantly when the tweak is injected into the game
 __attribute__((constructor)) static void initialize() {
-    // Listen for app launch
     CFNotificationCenterAddObserver(CFNotificationCenterGetLocalCenter(), NULL, &didFinishLaunching, (CFStringRef)UIApplicationDidFinishLaunchingNotification, NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
 }
