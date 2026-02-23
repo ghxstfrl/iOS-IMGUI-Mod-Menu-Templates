735defb28c89056d0086c7003e6c6ff50776f925
ghxstfrl <wymwdim@gmail.com>
2026-02-23 09:44:23 -0600

    Update PubgLoad.mm

--- Esp/PubgLoad.mm
+++ Esp/PubgLoad.mm
@@ -1,18 +1,41 @@
-- (void)toggleMenuFromButton {
-    NSLog(@"BUTTON PRESSED");
+- (UIButton *)buildMenuButton {
 
-    UIAlertController *alert =
-    [UIAlertController alertControllerWithTitle:@"Test"
-                                        message:@"Button Works"
-                                 preferredStyle:UIAlertControllerStyleAlert];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
 
-    UIAlertAction *ok =
-    [UIAlertAction actionWithTitle:@"OK"
-                             style:UIAlertActionStyleDefault
-                           handler:nil];
+    // Smaller rectangle
+    button.frame = CGRectMake(20, 120, 75, 36);
 
-    [alert addAction:ok];
+    // Rounded rectangle
+    button.layer.cornerRadius = 10;
+    button.clipsToBounds = YES;
+    button.tag = kM1ButtonTag;
 
-    UIWindow *window = [UIApplication sharedApplication].keyWindow;
-    [window.rootViewController presentViewController:alert animated:YES completion:nil];
+    // Gradient (Blue → Purple)
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = button.bounds;
+    gradient.colors = @[
+        (id)[UIColor colorWithRed:0.20 green:0.45 blue:1.0 alpha:0.85].CGColor,   // Blue
+        (id)[UIColor colorWithRed:0.55 green:0.20 blue:0.95 alpha:0.85].CGColor  // Purple
+    ];
+    gradient.startPoint = CGPointMake(0, 0);
+    gradient.endPoint = CGPointMake(1, 1);
+    gradient.cornerRadius = 10;
+
+    [button.layer insertSublayer:gradient atIndex:0];
+
+    // Text
+    [button setTitle:@"M1" forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont boldSystemFontOfSize:15];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+
+    // Slight border glow
+    button.layer.borderWidth = 1;
+    button.layer.borderColor = [[UIColor whiteColor] colorWithAlphaComponent:0.25].CGColor;
+
+    // Action
+    [button addTarget:self
+               action:@selector(toggleMenuFromButton)
+     forControlEvents:UIControlEventTouchUpInside];
+
+    return button;
 }
