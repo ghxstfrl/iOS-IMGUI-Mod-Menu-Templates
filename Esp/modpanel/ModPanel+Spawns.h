f150f836a45e8e7dd0505d9ed6c913bf8dba10b6
ghxstfrl <wymwdim@gmail.com>
2026-02-23 11:59:34 -0600

    Update ModPanel+Spawns.h

--- Esp/modpanel/ModPanel+Spawns.h
+++ Esp/modpanel/ModPanel+Spawns.h
@@ -1,1 +1,11 @@
+#import "ModPanel.h"
 
+@interface ModPanel (Spawns)
+
+// Spawn-related functions
+- (void)spawnHearts;
+- (void)spawnRain;
+- (void)startLoopedSpawns;
+- (void)updateSpawns;
+
+@end
