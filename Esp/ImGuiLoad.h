dfece555602cbd5178cdbd4398fea04bfd74d2d8
ghxstfrl <wymwdim@gmail.com>
2026-02-28 09:18:37 -0600

    Add ImGuiLoad interface for UI loading
    Add lightweight loader helper for GhostMenu UI

--- Esp/ImGuiLoad.h
+++ Esp/ImGuiLoad.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
 #import "modpanel/ModPanel.h"
 
+// lightweight loader helper for GhostMenu UI
 @class ImGuiDrawView;   // forward declaration ONLY
 
 @interface ImGuiLoad : NSObject
