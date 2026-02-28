8cfb8f1f6e58bec044a8def5efd680127e5e7d8c
ghxstfrl <wymwdim@gmail.com>
2026-02-28 09:18:59 -0600

    Add isMenuVisible property to ImGuiDrawView

--- Esp/ImGuiDrawView.h
+++ Esp/ImGuiDrawView.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
 #import <MetalKit/MetalKit.h>
 
+// GhostMenu rendering view
 @interface ImGuiDrawView : MTKView <MTKViewDelegate>
 @property (nonatomic, strong) id<MTLCommandQueue> commandQueue;
 @property (nonatomic, assign) BOOL isMenuVisible;
